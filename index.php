ce321aa18970d88313e443b2b568c36ec909fe33
newpond <poramen890@gmail.com>
2020-12-02 12:18:27 +0700

    two commit

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-lg-3"></div>
+            <div class="col-lg-6">
+                <form method="GET" action="./APTRequired.php" >
+                <div class="form-group">
+                  <label for="exampleFormControlFile1">select your images</label>
+                  <input type="file" name="file" class="form-control-file" id="exampleFormControlFile1">
+                </div>
+                <button type="submit" class="btn btn-primary">send</button>
+              </form></div>
+              <div class="col-lg-3"></div>
+        </div>
+    </div>
+    
+</body>
+</html>
+
 <script src="./script/script.js"></script>
 
